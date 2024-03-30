2f6910477ae6af8e65f472e117e7c3a3b117abf4
yzx <yuan@qq.com>
2024-03-30 15:19:45 +0800

    second commit

--- api-app2/src/d5_array/testgit.java
+++ api-app2/src/d5_array/testgit.java
@@ -3,5 +3,7 @@ package d5_array;
 public class testgit {
     public static void main(String[] args) {
         System.out.println("wox");
+        System.out.println("fox");
+        System.out.println("wxoc");
     }
 }
